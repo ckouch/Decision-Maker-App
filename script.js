1fba9086993c9a1327076978709378a23bd9d27a
Samied245 <139237825+Samied245@users.noreply.github.com>
2024-09-25 09:34:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,78 @@
-// var money = document.getElementById("money").value;
-// var hotorcold = document.getElementById("hotorcold").value;
-// console.log(money + hotorcold);
+
 var decision;
+var num;
+var pic="assets/";
+//create variables
+
+// generate a random number between 0 and 1
+function generateRandom(number){
+    number = Math.floor(Math.random()*2);
+    return number;
+}
+
+
+// update the screen
+function reset(){
+    document.getElementById("money").value = "";
+    document.getElementById("hotorcold").value = "";
+}
+
+
 function createDecision(){
+
+    // create two variables - money and hotorcold to store
+    //the value of money and whether the user likes hot or cold weather
     var money = document.getElementById("money").value;
     var hotorcold = document.getElementById("hotorcold").value;
-    if (money < 500) decision = "Stay home and get a job";
+
+    pic = "assets/";
+    if (money < 1){r
+        decision = "Please input a number";
+    }
+    else if (money < 500){ 
+        decision = "Stay home and get a job";
+        pic += "best-cry-ever-cry.gif";
+    }
     else if (money >= 500 && money <= 1500){
-        var num = Math.floor(Math.random() * 2);
+        num = generateRandom(num); // call function
         decision = "You should go to ";
         if (hotorcold == "hot"){
-            var place = ["Atlanta", "Jacksonville"];
-            decision += place[num];
+            var place = ["Atlanta, Georgia", "Jacksonville, Florida"];
+            var picture = ["atlanta.jpg", "jacksonville.jpg"];
+            decision += place[num]; //add place to the statement
+            pic += picture[num]; // add the picture link to show
         }
         else if (hotorcold == "cold"){
-            var place = ["Minneapolis", "Maine"];
+            var place = ["Minneapolis, Minnesota", "Maine"];
+            var picture = ["mineapollis.jpg", "maine.jpg"];
             decision += place[num];
+            pic += picture[num];
         }
     }
     else{
-        var num = Math.floor(Math.random() * 2);
+        num = generateRandom(num);
         decision = "You should go to ";
         if (hotorcold == "hot"){
-            var place = ["Miami", "Los Angeles"];
+            var place = ["Miami, Florida", "Los Angeles, California"];
+            var picture = ["miami.jpg", "la.jpg"];
             decision += place[num];
+            pic += picture[num];
         }
         else if (hotorcold == "cold"){
-            var place = ["Boston", "Seattle"];
+            var place = ["Boston, Massachusettes", "Seattle, Washington"];
+            var picture = ["boston.jpg", "seattle.jpg"];
             decision += place[num];
+            pic += picture[num];
         }
     }
-    console.log(decision);
+    // show the suggestion on the page
     document.getElementById("output").innerHTML = decision;
+    if (money > 0) document.getElementById("pic").src = pic;
+    //this make sure that there is no picture printed out if
+    //there are no money input
+
+
+    reset(); // call the function
 }
 
 
